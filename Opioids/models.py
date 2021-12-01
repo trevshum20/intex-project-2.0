58b6efcae43a9babd875efc2f06425601c60c7bc
Jared Christensen <90413445+Jared23546@users.noreply.github.com>
2021-12-01 15:55:16 -0700

    models look good now

--- Opioids/models.py
+++ Opioids/models.py
@@ -4,38 +4,51 @@ from django.db.models.fields import NOT_PROVIDED, GenericIPAddressField
 # Create your models here.
 
 class pd_prescriber(models.Model):
-    NPI = models.IntegerField(null=True, unique=True)
-    Fname = models.CharField(max_length=11, null=True)
-    Lname = models.CharField(max_length=11, null=True)
-    Gender = models.CharField(max_length=1, null=True)
-    State = models.CharField(max_length=2, null=True)
-    Credential = models.CharField(max_length=20)
-    Specialty = models.CharField(max_length=62, null=True)
-    IsOpioidPrescriber = models.CharField(max_length=5, null=True)
-    TotalPrescriptions = models.IntegerField(null=True)
+    fname = models.CharField(max_length=11, null=True)
+    lname = models.CharField(max_length=11, null=True)
+    gender = models.CharField(max_length=1, null=True)
+    state = models.CharField(max_length=2, null=True)
+    credentials = models.CharField(max_length=20)
+    specialty = models.CharField(max_length=62, null=True)
+    isopioidprescriber = models.CharField(max_length=5, null=True)
+    totalprescriptions = models.IntegerField(null=True)
 
     class Meta:
         db_table = "pd_prescriber"
     
     def __str__(self):
-        fullname = self.Lname + ", " + self.Fname
+        fullname = self.lname + ", " + self.fname
         return fullname
 
 class pd_drugs(models.Model) :
-    DrugName = models.CharField(max_length=30, null=True, unique=True)
-    IsOpioid = models.CharField(max_length=5, null=True)
+    drugname = models.CharField(max_length=30, null=True, unique=True)
+    isopioid = models.CharField(max_length=5, null=True)
     class Meta:
         db_table = "pd_drugs"
     
     def __str__(self):
-        return self.DrugName
+        return self.drugname
+
 class pd_triple(models.Model):
-    PrescriberID = models.ForeignKey(pd_prescriber, to_field="NPI", on_delete=models.CASCADE, null=True)
-    DrugName = models.ForeignKey(pd_drugs, to_field="DrugName", on_delete=models.CASCADE, null=True)
-    Qty = models.CharField(max_length=1, null=True)
+    prescriber = models.ForeignKey(pd_prescriber, to_field="id", on_delete=models.CASCADE, null=True)
+    drugname = models.ForeignKey(pd_drugs, to_field="drugname", on_delete=models.CASCADE, null=True)
+    qty = models.CharField(max_length=1, null=True)
 
     class Meta:
         db_table = "pd_triple"
 
     def __str__(self):
-        return self.Qty
+        return self.qty
+
+class pd_statedata(models.Model):
+    state = models.CharField(max_length=20, null=True)
+    stateabbrev = models.CharField(max_length=2, null=True)
+    population = models.IntegerField(null=True)
+    deaths = models.IntegerField(null=True)
+    class Meta:
+        db_table = "pd_statedata"
+
+    def __str__(self):
+        return str(self.deaths)
+
+        
