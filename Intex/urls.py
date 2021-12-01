9f719c91a80375001f7dfdbd255546822050753b
bder1999 <bder1999@byu.edu>
2021-12-01 15:57:21 -0700

    for trevor

--- Intex/urls.py
+++ Intex/urls.py
@@ -20,6 +20,7 @@ from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', include('Opioids.urls')),
 ]
