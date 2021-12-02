ec8b665dc94e57746abab22e8d189f8e20de5d91
bder1999 <bder1999@byu.edu>
2021-12-02 15:15:29 -0700

    push

--- Opioids/urls.py
+++ Opioids/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, drugsPageView, prescribersPageView, prescriberInfoPageView
 
 urlpatterns = [
+    path('prescriberinfo/<int:prescriber_id>/', prescriberInfoPageView, name='prescriberInfo'),
+    path('prescribers/', prescribersPageView, name='prescribers'),
+    path('drugs/', drugsPageView, name='drugs'),
     path('', indexPageView, name="index"),
 ]
--- Opioids/views.py
+++ Opioids/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from .models import pd_drugs, pd_prescriber
 #from .models import
 
 # Create your views here.
+
+def drugsPageView(request) :
+    drugs = pd_drugs.objects.all()
+
+    context = {
+        "drugs": drugs,
+    }
+
+    return render(request, 'Opioids/drugs.html', context)
+
+def prescribersPageView(request) :
+    prescribers = pd_prescriber.objects.all()
+
+    context = {
+        "prescribers": prescribers,
+    }
+
+    return render(request, 'Opioids/prescribers.html', context)
+
+def prescriberInfoPageView(request, prescriber_id) :
+    prescribers = pd_prescriber.objects.get(id = prescriber_id)
+
+    context = {
+        "prescribers": prescribers,
+    }
+    return render(request, 'Opioids/prescriberinfo.html')
 
 def indexPageView(request) :
     return render(request, 'Opioids/index.html')
