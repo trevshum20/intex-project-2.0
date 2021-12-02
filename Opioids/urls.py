492c9b11195eef042540e8cbfdbb8d7f38185115
Madison Clark <madamber4@gmail.com>
2021-12-02 15:03:07 -0700

    stuff

--- Opioids/urls.py
+++ Opioids/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import indexPageView, searchPageView
 
 urlpatterns = [
+    path("search/", searchPageView, name="search"),
     path('', indexPageView, name="index"),
-    path("search/", searchPageView, name="search")
 ]
