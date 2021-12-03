96190c94cd488e84c9243ae3f4770247d4671cfa
bder1999 <bder1999@byu.edu>
2021-12-02 17:33:08 -0700

    works baby

--- Opioids/urls.py
+++ Opioids/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import indexPageView, drugsPageView, prescribersPageView, prescriberInfoPageView, searchPageView
 
 urlpatterns = [
-    path('prescriberinfo/', prescriberInfoPageView, name='prescriberInfo'),
+    path('prescriberinfo/<int:prescriber_id>/', prescriberInfoPageView, name='prescriberInfo'),
     path('prescribers/', prescribersPageView, name='prescribers'),
     path('drugs/', drugsPageView, name='drugs'),
     path("search/", searchPageView, name="search"),
