77d2f5648a96d48a1c760a7bdc22f98fe95f9928
Madison Clark <madamber4@gmail.com>
2021-12-02 21:25:35 -0700

    updating

--- Opioids/views.py
+++ Opioids/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Drug, Prescriber
+from .models import Drug, Prescriber, Prescriber_Drug
 #from .models import
 from Opioids.models import Prescriber
 from django.db.models import Q
@@ -16,7 +16,7 @@ def drugsPageView(request) :
     return render(request, 'Opioids/drugs.html', context)
 
 def prescribersPageView(request) :
-    prescribers = Prescriber.objects.all()
+    prescribers = Prescriber.objects.all()[:5]
 
     context = {
         "prescribers": prescribers,
@@ -26,9 +26,12 @@ def prescribersPageView(request) :
 
 def prescriberInfoPageView(request, prescriber_id) :
     prescribers = Prescriber.objects.get(id = prescriber_id)
+    prescriber_drug = Prescriber_Drug.objects.filter(prescriber = prescriber_id)
+    print(prescriber_drug)
 
     context = {
         "prescribers": prescribers,
+        "prescriber_drug": prescriber_drug
     }
     return render(request, 'Opioids/prescriberinfo.html', context)
 
