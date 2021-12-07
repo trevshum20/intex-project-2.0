2093316abf9a402af87048f9ad9c78efaad0a6d9
trevshum20 <trevshum20@gmail.com>
2021-12-06 22:31:33 -0700

    dep

--- Intex/settings.py
+++ Intex/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'HOST': 'byuintex2021.postgres.database.azure.com',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
